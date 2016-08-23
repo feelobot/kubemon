9894b558ba08772b2cd35bd4cb4705ecc0978ba9
feelobot <felix.a.rod@gmail.com>
2016-08-23 18:14:43 +0200

    Working

---
+++ kubemon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+kubectl create -f deploy/
+echo "Waiting for things to come up..."
+sleep 10
+curl "http://admin:secret@`minikube service grafana --url | sed 's/http\:\/\///' `/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"influx","type":"influxdb","url":"http://influxdb.default.svc.cluster.local:8086/","access":"proxy","isDefault":true,"database":"k8s","user":"","password":""}'
+curl -d @dashboard.json "http://admin:secret@`minikube service grafana --url | sed 's/http\:\/\///' `/api/dashboards/db" -X POST -H 'Content-Type: application/json;charset=UTF-8'
+minikube service grafana

ff63f808188a03905ed667b5ea138dcb880e797d
feelobot <felix.a.rod@gmail.com>
2016-08-24 18:03:44 +0200

    fixit

--- kubemon.sh
+++ kubemon.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+kubectl create -f deploy/influxdb-deployment.yaml
+kubectl create -f deploy/influxdb-service.yaml
+kubectl create -f deploy/influxdb-ingress.yaml
+sleep 5
 kubectl create -f deploy/
 echo "Waiting for things to come up..."
 sleep 10
 curl "http://admin:secret@`minikube service grafana --url | sed 's/http\:\/\///' `/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"influx","type":"influxdb","url":"http://influxdb.default.svc.cluster.local:8086/","access":"proxy","isDefault":true,"database":"k8s","user":"","password":""}'
-sleep 10
 curl -d @dashboard.json "http://admin:secret@`minikube service grafana --url | sed 's/http\:\/\///' `/api/dashboards/db" -X POST -H 'Content-Type: application/json;charset=UTF-8'
 echo ""
 minikube service grafana

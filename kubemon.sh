e753f3c10d53b67659a6bfb5d0ef032b3f62012c
feelobot <felix.a.rod@gmail.com>
2016-08-29 18:01:07 +0200

    cluster stats

--- kubemon.sh
+++ kubemon.sh
@@ -2,8 +2,15 @@
 kubectl create -f deploy/influxdb-deployment.yaml
 kubectl create -f deploy/influxdb-service.yaml
 kubectl create -f deploy/influxdb-ingress.yaml
+echo "Waiting for influxdb..."
 sleep 5
 kubectl create -f deploy/
+#kubectl create -f deploy/heapster-service.yaml
+#kubectl create -f deploy/grafana-deployment.yaml
+#kubectl create -f deploy/grafana-service.yaml
+#kubectl create -f deploy/grafana-ingress.yaml
+#kubectl create -f deploy/kapacitor-deployment.yaml
+#kubectl create -f deploy/kapacitor-service.yaml
 echo "Waiting for things to come up..."
 sleep 10
 curl "http://admin:secret@`minikube service grafana --url | sed 's/http\:\/\///' `/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"influx","type":"influxdb","url":"http://influxdb.default.svc.cluster.local:8086/","access":"proxy","isDefault":true,"database":"k8s","user":"","password":""}'
